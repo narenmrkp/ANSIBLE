d9ca9e6eb155f8c275b2beb592c92e2c2eca3d37
narenmrkp <159535924+narenmrkp@users.noreply.github.com>
2024-02-14 20:09:54 +0530

    Update install_jenkins.sh

--- Jenkins-Ec2/install_jenkins.sh
+++ Jenkins-Ec2/install_jenkins.sh
@@ -10,4 +10,4 @@ mkdir Ansible && cd Ansible
 pwd
 git clone https://github.com/narenmrkp/ANSIBLE.git
 cd ANSIBLE
-ansible-playbook -i localhost Jenkins.yml
+ansible-playbook -i localhost Jenkins-playbook.yml
